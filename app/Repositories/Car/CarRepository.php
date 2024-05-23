c36a8aa36a38b1e26dcb93ad35653165613125ba
Cleifson <cl.raujo1@gmail.com>
2024-05-22 22:39:06 -0300

    feat: criando estruturas para comunicação HTTPS Car

---
+++ app/Repositories/Car/CarRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Repositories\Car;
+
+use App\Models\Car;
+
+class CarRepository implements CarRepositoryInterface
+{
+    public function create(array $data)
+    {
+        return Car::create($data);
+    }
+
+}
---
+++ app/Services/CarService.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\Car\CarRepositoryInterface;
+
+class CarService
+{
+    protected $carRepository;
+
+    public function __construct(CarRepositoryInterface $carRepository)
+    {
+        $this->carRepository = $carRepository;
+    }
+
+    public function createCar(array $data)
+    {
+        return $this->carRepository->create($data);
+    }
+}
