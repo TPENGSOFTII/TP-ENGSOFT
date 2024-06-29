dea5fa28bd76b2e2870ea8f9f5fd280260e14f8a
vitorugoc <vitorhugo02cc@gmail.com>
2024-06-29 19:42:57 -0300

    test: adicionando testes ao CarController

--- tp-engsoft/app/Models/Car.php
+++ tp-engsoft/app/Models/Car.php
@@ -1,20 +1,19 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Car extends Model
 {
     use SoftDeletes;
+    use HasFactory;
 
     protected $fillable = [
         'brand', 'model', 'license_plate', 'color',
     ];
-
     protected $dates = ['deleted_at'];
-
     public function users()
     {
         return $this->belongsToMany(Car::class, 'car_user');
